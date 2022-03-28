280ea006bc81f4f1050e9ae686035374afda656c
Sergo <Sergo>
2022-03-28 18:45:31 +0300

    доработка класса роутер

--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -2,35 +2,51 @@
 
 namespace Framework\Http\Router;
 
+use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Psr\Http\Message\ServerRequestInterface;
 
 class Router
 {
-    private $routes;
+    private RouteCollection $routes;
 
     public function __construct(RouteCollection $routes)
     {
         $this->routes = $routes;
     }
 
-//    public function match(ServerRequestInterface $request) : Result
-//    {
-//        foreach ($this->routes->getRoutes() as $route){
-//            if(){
-//
-//            }
-//
-//            $patern
-//
-//            if(){
-//
-//            }
-//        }
-//    }
+    public function match(ServerRequestInterface $request) : Result
+    {
+        foreach ($this->routes->getRoutes() as $route){
+            if($route->methods && !\in_array($request->getMethod(), $route->methods, true)){
+                continue;
+            }
+            $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use ($route) {
+                $argument = $matches[1];
+                $replace = $route->tokens[$argument] ?? '[^}]+';
+                return '(?P<' . $argument . '>' . $replace . ')';
+            }, $route->parattern);
+            if (preg_match($pattern, $request->getUri()->getPath(), $matches)) {
+                return new Result(
+                  $route->name,
+                  $route->handler,
+                  array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+                );
+            }
+        }
+        throw new RequestNotMatchedException($request);
+    }
 //
 //    public function generate($name, array $params = []) : string
 //    {
 //
 //    }
+
+    /**
+     * @return RouteCollection
+     */
+    public function getRoutes(): RouteCollection
+    {
+        return $this->routes;
+    }
 
 }
