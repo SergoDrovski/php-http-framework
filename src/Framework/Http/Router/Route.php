819aec65c5ca2208d98855807ca0bdb9a81392fd
Сергей <sergiodrovski@gmail.com>
2022-03-31 22:38:59 +0300

    .

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -2,6 +2,8 @@
 
 namespace Framework\Http\Router;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
     public $name;
@@ -17,5 +19,50 @@ class Route
         $this->handler = $handler;
         $this->methods = $methods;
         $this->tokens = $tokens;
+    }
+
+    public function match(ServerRequestInterface $request): ?Result
+    {
+            // Проверяем совпадение метода в запросе с методами маршрутов
+            if($this->methods && !\in_array($request->getMethod(), $this->methods, true)){
+                return null;
+            }
+
+            // Получаем рег.выражение для извлечения пути из URLa
+
+            $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) {
+                $argument = $matches[1];
+                $replace = $this->tokens[$argument] ?? '[^}]+';
+                return '(?P<' . $argument . '>' . $replace . ')';
+            }, $this->pattern);
+
+            $addSlashesPattern = addcslashes($pattern, '/');
+            $path = $request->getUri()->getPath();
+
+            if (preg_match("/{$addSlashesPattern}/m", $path, $matches)) {
+                return new Result(
+                    $this->name,
+                    $this->handler,
+                    array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+                );
+            }
+            return null;
+    }
+
+    public function generate($name, array $params = []): ?string
+    {
+        $arguments = array_filter($params);
+        // Проверяем совпадение имени роута в коллекции маршрутов
+        if ($name !== $this->name) {
+            return null;
+        }
+        // с помощью рег.выражения получаем реальный URL с аргументами вместо маски
+        return preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
+            $argument = $matches[1];
+            if (!array_key_exists($argument, $arguments)) {
+                return new \InvalidArgumentException("Missing parameter {$argument}");
+            }
+            return $arguments[$argument];
+        }, $this->pattern);
     }
 }
--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -19,29 +19,9 @@ class Router
     public function match(ServerRequestInterface $request)
     {
         foreach ($this->routes->getRoutes() as $route){
-            // Проверяем совпадение метода в запросе с методами маршрутов
-
-            if($route->methods && !\in_array($request->getMethod(), $route->methods, true)){
-                continue;
-            }
-
-            // Получаем рег.выражение для извлечения пути из URLa
-
-            $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use ($route) {
-            $argument = $matches[1];
-            $replace = $route->tokens[$argument] ?? '[^}]+';
-            return '(?P<' . $argument . '>' . $replace . ')';
-            }, $route->pattern);
-
-            $addSlashesPattern = addcslashes($pattern, '/');
-            $path = $request->getUri()->getPath();
-
-            if (preg_match("/{$addSlashesPattern}/m", $path, $matches)) {
-                return new Result(
-                  $route->name,
-                  $route->handler,
-                  array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
-                );
+            $result = $route->match($request);
+            if($result){
+                return $result;
             }
         }
         throw new RequestNotMatchedException($request);
@@ -49,25 +29,10 @@ class Router
 
     public function generate($name, array $params = []) : string
     {
-        $arguments = array_filter($params);
         foreach ($this->routes->getRoutes() as $route){
-            // Проверяем совпадение имени роута в коллекции маршрутов
-            if($name !== $route->name){
-                continue;
-            }
-
-            // с помощью рег.выражения получаем реальный URL с аргументами вместо маски
-
-            $url = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
-                $argument = $matches[1];
-                if(!array_key_exists($argument, $arguments)){
-                    return new \InvalidArgumentException("Missing parameter {$argument}");
-                }
-                return $arguments[$argument];
-            }, $route->pattern);
-
-            if ($url !== null) {
-                return $url;
+            $result = $route->generate($name, $params);
+            if($result !== null){
+                return $result;
             }
         }
         throw new RoutNotFoundException($name, $params);
